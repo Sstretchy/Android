140a825b1141b8cafae6c9aed70b8e0e544b4db5
Sstretchy <garshina.olya1997@gmail.com>
2019-12-25 23:57:50 +0300

    ura

--- components/alert.js
+++ components/alert.js
@@ -1,22 +1,65 @@
 import React, { Component } from 'react';
 import { View, ScrollView } from 'react-native';
-import { Button, Paragraph, Dialog, Portal } from 'react-native-paper';
-import { Avatar, Card, Title, Chip } from 'react-native-paper';
-import { Input, Icon, Item } from 'native-base';
+import { Button, Chip, Dialog, Portal } from 'react-native-paper';
+import { Input, Icon, Item, Textarea } from 'native-base';
 
 export default class Alert extends Component {
   state = {
     text: '',
+    date: '',
     inputTag: '',
     tags: [],
   };
+
+  componentWillReceiveProps(nextProps) {
+    const { indexEdit, isEdit, notes } = nextProps;
+    if (isEdit) {
+      const notesArray = notes.slice();
+      const note = notesArray.splice(indexEdit, 1);
+      this.setState({
+        text: note[0].text,
+        date: note[0].date,
+        tags: note[0].tags,
+        inputTag: '',
+      });
+    } else {
+      this.setState({
+        text: '',
+        date: '',
+        inputTag: '',
+        tags: [],
+      });
+    }
+  }
 
   onTextChange = (text, name) => {
     this.setState({ [name]: text });
   };
 
+  addTag = () => {
+    let tagsArray = this.state.tags;
+    tagsArray.push(this.state.inputTag);
+    this.setState({ tags: tagsArray, inputTag: '' });
+  }
+
+  removeTag = (index) => {
+    let tagsArray = this.state.tags;
+    tagsArray.splice(index, 1);
+    this.setState({ tags: tagsArray });
+  }
+
+  onSaveNote = () => {
+    this.props.onSave(this.state)
+    this.setState({
+      text: '',
+      inputTag: '',
+      tags: [],
+    })
+    this.props.hideDialog();
+  }
+
   render() {
-    const { visible, hideDialog, showDialog, onSave } = this.props;
+    const { visible, hideDialog } = this.props;
     const { text, inputTag, tags } = this.state
     return (
       <View>
@@ -24,33 +67,45 @@ export default class Alert extends Component {
           <Dialog
             visible={visible}
             onDismiss={hideDialog}>
-            <Dialog.Title>Alert</Dialog.Title>
-            <Dialog.Content style={{ maxHeight: 400, minHeight: 200 }}>
-              <ScrollView>
-                <Item>
-                  <Input
-                    multiline
-                    bordered placeholder="Write your note"
-                    onTextChange={text => this.onTextChange(text, 'text')}
-                    value={text}
-                  />
-                </Item>
-                <Item style={{ marginTop: 20 }}>
-                  <Input
-                    multiline
-                    bordered placeholder="Add tags"
-                    onTextChange={text => this.onTextChange(text, 'inputTag')}
-                    value={inputTag}
-                  />
-                  <Button transparent>
-                    <Icon style={{ color: '#6200ee' }} name='add' />
-                  </Button>
-                </Item>
+            <Dialog.Title>Your note</Dialog.Title>
+            <Dialog.Content style={{ maxHeight: 400, minHeight: 400 }}>
+              <Textarea
+                rowSpan={4}
+                bordered placeholder="Write your note"
+                onChangeText={text => this.onTextChange(text, 'text')}
+                value={text}
+              />
+              <Item style={{ marginTop: 20 }}>
+                <Input
+                  bordered placeholder="Add tags"
+                  onChangeText={text => this.onTextChange(text, 'inputTag')}
+                  value={inputTag}
+                />
+                <Icon onPress={this.addTag} style={{ fontSize: 30, color: '#6200ee' }} name='add-circle' />
+              </Item>
+              <ScrollView >
+                <View style={{ flex: 1, alignItems: 'flex-start' }}>
+                  {tags.map((tag, index) => (
+                    <Chip
+                      onClose={() => this.removeTag(index)}
+                      key={index}
+                      style={{ marginTop: 10 }}
+                    >
+                      {tag}
+                    </Chip>
+                  ))}
+                </View>
               </ScrollView>
             </Dialog.Content>
             <Dialog.Actions>
               <Button onPress={hideDialog}>Cancel</Button>
-              <Button onPress={onSave} style={{ marginLeft: 'auto' }}>Save</Button>
+              <Button
+                disabled={!text}
+                onPress={this.onSaveNote}
+                style={{ marginLeft: 'auto' }}
+              >
+                Save
+              </Button>
             </Dialog.Actions>
           </Dialog>
         </Portal >
--- components/body.js
+++ components/body.js
@@ -1,43 +1,83 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
-import { TextInput } from 'react-native';
-import { Container, Header, Input, Content, Textarea, Label, Fab, Footer, FooterTab, Left, Right, Body, Icon, Text, Item } from 'native-base';
+import { ScrollView, View } from 'react-native';
+import { Const } from '../constants';
+import { Container, Header, Input, Content, Fab, Body, Icon, Text, Item } from 'native-base';
 import Alert from './alert';
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import { Avatar, Button, Card, Title, Paragraph, Chip } from 'react-native-paper';
 import { AsyncStorage } from 'react-native';
 
 export default class BodyComponent extends Component {
   state = {
+    filteredNotes: [],
     notes: [],
     serchValue: '',
-    visible: false
+    visible: false,
+    isEdit: false,
+    indexEdit: '',
   };
 
   async componentDidMount() {
     try {
       const value = await AsyncStorage.getItem('notes');
       if (value !== null) {
-        console.log(value);
+        this.setState({ notes: JSON.parse(value), filteredNotes: JSON.parse(value), })
       } else {
-        await AsyncStorage.setItem('notes', []);
+        await AsyncStorage.setItem('notes', "[]");
       }
     } catch (error) {
-      // Error retrieving data
+      throw new Error('Уупс!');
     }
   }
+
+  clearSearch = () => this.setState({ serchValue: '', filteredNotes: this.state.notes });
 
   showDialog = () => this.setState({ visible: true });
 
   hideDialog = () => this.setState({ visible: false });
 
-  onSave = () => {
+  onSave = async (note) => {
+    const myNotes = this.state.notes.slice();;
+    if (this.state.isEdit) {
+      myNotes.splice(this.state.indexEdit, 1);
+    }
+    myNotes.unshift({ text: note.text, tags: note.tags, date: new Date() });
+    this.setState({ notes: myNotes, filteredNotes: myNotes, isEdit: false, indexEdit: '' });
+    await AsyncStorage.setItem('notes', JSON.stringify(myNotes));
+    this.rerenderNotes(this.state.serchValue);
+    this.hideDialog();
+  }
 
+  editNote = (index) => {
+    this.setState({ isEdit: true, indexEdit: index });
+    this.showDialog();
+  }
+
+  deleteNote = async (index) => {
+    const myNotes = this.state.notes;
+    myNotes.splice(index, 1);
+    this.setState({ notes: myNotes, filteredNotes: myNotes });
+    await AsyncStorage.setItem('notes', JSON.stringify(myNotes));
   }
 
   onSearchTextChange = text => {
+    this.rerenderNotes(text);
     this.setState({ serchValue: text });
   };
+
+  rerenderNotes = (text) => {
+    const filtered = []
+
+    for (const note of this.state.notes) {
+      for (const tag of note.tags) {
+        if (tag.match(text)) {
+          filtered.push(note);
+        }
+      }
+    }
+    this.setState({ filteredNotes: filtered });
+  }
   render() {
+    const { filteredNotes, notes, serchValue } = this.state;
     return (
       <Container>
         <Header style={{ backgroundColor: '#6200ee' }}>
@@ -47,35 +87,59 @@ export default class BodyComponent extends Component {
             </Title>
           </Body>
         </Header>
+        <Item>
+          <Input
+            bordered
+            onChangeText={text => this.onSearchTextChange(text)}
+            value={this.state.serchValue} placeholder="Search by tag"
+          />
+          <Button transparent onPress={this.clearSearch} >
+            <Icon style={{ fontSize: 30, color: '#6200ee' }} name='close' />
+          </Button>
+        </Item>
         <ScrollView>
           <Content>
-            <Item>
-              <Input
-                bordered
-                onSearchTextChange={text => this.onSearchTextChange(text)}
-                value={this.state.serchValue} placeholder="Search by tag"
-              />
-              <Button transparent>
-                <Icon style={{ fontSize: 35, color: '#6200ee' }} name='close' />
-              </Button>
-            </Item>
-            <Card style={{ backgroundColor: '#be8fff', margin: 10 }}>
-              <Card.Title
-                title="Card Title"
-                left={(props) => <Avatar.Icon {...props} style={{ fontSize: 35, color: 'darkblue' }} icon="heart"
-                />}
-              />
-              <Card.Content style={{ backgroundColor: '#eeebff', padding: 20 }}>
-                <Paragraph>Card content </Paragraph>
-              </Card.Content>
-              <Card.Content style={{ backgroundColor: '#eeebff', padding: 20 }}>
-                <Paragraph>Card content </Paragraph>
-              </Card.Content>
-              <Card.Actions style={{ backgroundColor: '#332863' }} >
-                <Button style={{ paddingLeft: 30 }} ><Icon style={{ fontSize: 35, color: 'white' }} name='create' /></Button>
-                <Button style={{ marginLeft: 'auto', paddingRight: 30 }}><Icon style={{ fontSize: 35, color: 'white' }} name='trash' /></Button>
-              </Card.Actions>
-            </Card>
+            {!serchValue && filteredNotes.length === 0 ? <Text>Have no notes</Text> : <></>}
+            {filteredNotes.map((note, index) => (
+              <Card
+                key={index}
+                style={{ backgroundColor: '#be8fff', margin: 10 }}
+              >
+                <Card.Title
+                  title={`${new Date(note.date).getDate() < 10 ? '0' + new Date(note.date).getDate()
+                    : new Date(note.date).getDate()} ${Const.months[new Date(note.date).getMonth()]} ${new Date(note.date).getFullYear()} at ${new Date(note.date).getHours()}:${new Date(note.date).getMinutes()}`}
+                  left={(props) => <Avatar.Icon {...props} style={{ fontSize: 35, color: 'darkblue' }} icon="heart"
+                  />}
+                />
+                <Card.Content style={{ backgroundColor: '#eeebff', padding: 20 }}>
+                  <Paragraph style={{ fontSize: 17 }}>{note.text}</Paragraph>
+                </Card.Content>
+                <Card.Content style={{ backgroundColor: '#eeebff', padding: 20 }}>
+                  <View style={{ flex: 1, alignItems: 'flex-start' }}>
+                    {note.tags.map((tag, index) => (
+                      <Chip
+                        key={index}
+                        style={{ backgroundColor: '#be8fff', marginBottom: 7 }}
+                      >{tag}</Chip>
+                    ))}
+                  </View>
+                </Card.Content>
+                <Card.Actions style={{ backgroundColor: '#332863' }} >
+                  <Button
+                    style={{ paddingLeft: 30 }}
+                    onPress={() => this.editNote(index)}
+                  >
+                    <Icon style={{ fontSize: 30, color: 'white' }} name='create' />
+                  </Button>
+                  <Button
+                    style={{ marginLeft: 'auto', paddingRight: 30 }}
+                    onPress={() => this.deleteNote(index)}
+                  >
+                    <Icon style={{ fontSize: 30, color: 'white' }} name='trash' />
+                  </Button>
+                </Card.Actions>
+              </Card>
+            ))}
           </Content>
         </ScrollView>
         <Fab
@@ -93,6 +157,9 @@ export default class BodyComponent extends Component {
           hideDialog={this.hideDialog}
           showDialog={this.showDialog}
           onSave={this.onSave}
+          isEdit={this.state.isEdit}
+          indexEdit={this.state.indexEdit}
+          notes={this.state.notes}
         />
       </Container>
     );
